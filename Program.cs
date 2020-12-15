c6ff23e1a93b31a76f6e7c3079679dd981e69761
ajja94 <anja@getacademy.no>
2020-12-15 11:10:42 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InneUkeDesember2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InneUkeDesember2020
+{
+    class Rectangle
+    {
+ 
+
+        public Rectangle()
+        {
+            
+        }
+    }
+}
