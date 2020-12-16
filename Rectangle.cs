4d5e3320321829646573092833b42690a956546e
ajja94 <anja@getacademy.no>
2020-12-16 10:07:43 +0100

    :+1:

--- Rectangle.cs
+++ Rectangle.cs
@@ -7,10 +7,19 @@ namespace InneUkeDesember2020
     class Rectangle
     {
         private string _rectanglething;
+        private int _position1;
+        private int _position2;
+        private int _heigth;
+        private int _width;
+        private int position1;
 
-        public Rectangle(string rectanglething)
+        public Rectangle(string rectanglething, int position1, int position2, int heigth, int width)
         {
             _rectanglething = rectanglething;
+            _position1 = position1;
+            _position2 = position2;
+            _heigth = heigth;
+            _width = width;
         }
 
         public void Show()
