3f9b03aef768a12d53c4a9891a31abcb73d1d023
ajja94 <anja@getacademy.no>
2021-01-06 13:04:53 +0100

    :alien:

--- Program.cs
+++ Program.cs
@@ -4,11 +4,12 @@ namespace InneUkeDesember2020
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            var draw = new Rectangle("\u2592", 13, 2, 5, 6);
-            var userinput = Console.ReadKey();
-            Console.WriteLine(userinput);
+            var draw = new Rectangle("\u2592", 1, 3, 5, 6, ConsoleColor.Magenta);
+            //var userinput = Console.ReadKey();
+            //Console.Write(userinput);
             draw.Show();
         }
     }
--- Rectangle.cs
+++ Rectangle.cs
@@ -11,27 +11,31 @@ namespace InneUkeDesember2020
         private int _position2;
         private int _heigth;
         private int _width;
+        private ConsoleColor _consoleColor;
 
-        public Rectangle(string rectanglething, int position1, int position2, int heigth, int width)
+        public Rectangle(string rectanglething, int position1, int position2, int heigth, int width, ConsoleColor consoleColor)
         {
             _rectanglething = rectanglething;
             _position1 = position1;
             _position2 = position2;
             _heigth = heigth;
             _width = width;
+            _consoleColor = consoleColor;
         }
 
         public void Show()
         {
-            Console.CursorTop = _position1;
-            Console.CursorLeft = _position2;
-            Console.WriteLine(_rectanglething);
-            Console.SetCursorPosition(_position1, _position2);
+            DrawRectangle(_position1, _position2, ConsoleColor.Green);
+ 
+            //Console.SetCursorPosition(_position1, _position2);
         }
 
-        public void DrawRectangle()
+        public void DrawRectangle(int _position1, int _position2, ConsoleColor consoleColor)
         {
-            
+            Console.CursorLeft = _position1;
+            Console.CursorTop = _position2;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(_rectanglething);
         }
     }
 }
