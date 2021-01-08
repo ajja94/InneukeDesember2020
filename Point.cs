8f9cc82f2e116a69d222c883abdd1f87e2576f46
ajja94 <anja@getacademy.no>
2021-01-08 12:42:21 +0100

    :a:

---
+++ Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InneUkeDesember2020
+{
+    class Point
+    {
+        int dX = 0;
+        int dY = 0;
+
+        public  Point PointFromKey(ConsoleKeyInfo keyInfo)
+        {
+            if (keyInfo.Key == ConsoleKey.LeftArrow) dX--;
+            else if (keyInfo.Key == ConsoleKey.RightArrow) dX++;
+            else if (keyInfo.Key == ConsoleKey.UpArrow) dY--;
+            else if (keyInfo.Key == ConsoleKey.DownArrow) dY++;
+
+            return Point;
+        }
+    }
+}
